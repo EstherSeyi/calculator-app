138e2a4d5bfb71b760d5ceeca6eaa8f3399dc6f8
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-03 19:25:37 +0100

    updates app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
+import HeaderSection from "./components/HeaderSection";
+import InputSection from "./components/InputSection";
+import ButtonSection from "./components/ButtonSection";
+
 function App() {
-  return <div className="text-misc-white">Hello</div>;
+  return (
+    <div className="text-misc-white min-h-screen">
+      <div className="w-11/12 max-w-lg mx-auto mt-32">
+        <HeaderSection />
+        <InputSection />
+        <ButtonSection />
+      </div>
+    </div>
+  );
 }
 
 export default App;
