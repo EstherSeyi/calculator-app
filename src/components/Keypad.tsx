4cdda73bf80fdbad100b540bbf1ecd7c4bc9a013
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-03 21:29:55 +0100

    updates keypad component

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,7 +1,13 @@
+import keys from "../constants/keys";
+
+import Button from "./Button";
+
 const Keypad = () => {
   return (
-    <div>
-      <p>Keypad</p>
+    <div className="grid grid-cols-4 gap-4 bg-bg-element p-4 rounded">
+      {keys.map((key, index) => (
+        <Button key={`${key.value}_${index}`}>{key.label}</Button>
+      ))}
     </div>
   );
 };
