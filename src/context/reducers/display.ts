9ea45ce0832455e0be3ab2cd54e1d84461d7928f
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-04 17:05:40 +0100

    ensures that non numbers and zeros do not appear when clicked first

--- src/context/reducers/display.ts
+++ src/context/reducers/display.ts
@@ -5,15 +5,31 @@ type ActionType = {
   payload?: string;
 };
 
+const nonNumbers = ["-", "/", "x", "=", "+", "0"];
+
+const handleType = (state: typeof initialState, data?: string) => {
+  if (state.displayData === "" && data && nonNumbers.includes(data)) {
+    return state;
+  } else {
+    return {
+      ...state,
+      displayData: `${state.displayData}${data}`,
+    };
+  }
+};
+
 const displayReducer = (
   state = initialState,
   { type, payload }: ActionType
 ) => {
   switch (type) {
     case "TYPE_DATA":
+      return handleType(state, payload);
+
+    case "CLEAR_DATA":
       return {
         ...state,
-        displayData: `${state.displayData}${payload}`,
+        displayData: state.displayData.slice(0, state.displayData.length - 1),
       };
     case "RESET_DATA":
       return {
