e32d58d9cfe78b7f91841c6b28098cf610b58adc
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-04 15:21:34 +0100

    modifies app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import Header from "./components/Header";
 import Display from "./components/Display";
 import Keypad from "./components/Keypad";
@@ -9,7 +7,7 @@ function App() {
     <div className="text-misc-white">
       <div className="w-11/12 max-w-lg mx-auto mt-32">
         <Header />
-        <Display data="399,981" />
+        <Display />
         <Keypad />
       </div>
     </div>
