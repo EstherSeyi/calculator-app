ebebfe7306c06307a3c93040667531e550d14738
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-04 15:24:06 +0100

    modifies keypad component

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,12 +1,28 @@
-import keys from "../constants/keys";
-
 import Button from "./Button";
 
+import keys from "../constants/keys";
+import { useDisplay } from "../context/display";
+
 const Keypad = () => {
+  const { clear, type } = useDisplay();
+
+  const handleType = (data: string) => {
+    type(data);
+  };
+
+  const handleClear = () => {
+    clear();
+  };
+
   return (
     <div className="grid grid-cols-4 gap-x-2 gap-y-3 micro:gap-4  bg-bg-element p-4 rounded">
-      {keys.map((key, index) => (
-        <Button key={`${key.value}_${index}`}>{key.label}</Button>
+      {keys.map(({ value, label }, index) => (
+        <Button
+          key={`${value}_${index}`}
+          onClick={value === "del" ? handleClear : () => handleType(value)}
+        >
+          {label}
+        </Button>
       ))}
     </div>
   );
