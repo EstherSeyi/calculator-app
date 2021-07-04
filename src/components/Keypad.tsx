9353d5c46f421ffc1efbbdd130a470ebee9c14f7
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-04 17:04:08 +0100

    adds reset calculator functionality to keypad component

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -4,7 +4,7 @@ import keys from "../constants/keys";
 import { useDisplay } from "../context/display";
 
 const Keypad = () => {
-  const { clear, type } = useDisplay();
+  const { clear, type, reset } = useDisplay();
 
   const handleType = (data: string) => {
     type(data);
@@ -14,12 +14,22 @@ const Keypad = () => {
     clear();
   };
 
+  const handleReset = () => {
+    reset();
+  };
+
   return (
     <div className="grid grid-cols-4 gap-x-2 gap-y-3 micro:gap-4  bg-bg-element p-4 rounded">
       {keys.map(({ value, label }, index) => (
         <Button
           key={`${value}_${index}`}
-          onClick={value === "del" ? handleClear : () => handleType(value)}
+          onClick={
+            value === "del"
+              ? handleClear
+              : value === "reset"
+              ? handleReset
+              : () => handleType(value)
+          }
         >
           {label}
         </Button>
