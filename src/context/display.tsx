e4093a8fa793b0ad7876bb0afc0cb7c4077d50ae
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-04 15:24:21 +0100

    adds display context

---
+++ src/context/display.tsx
@@ -0,0 +1,30 @@
+import { createContext, useReducer, useContext, ReactNode } from "react";
+
+import displayReducer from "./reducers/display";
+import { typeData, clearData } from "./actions/display";
+
+export const initialState = {
+  displayData: "0",
+};
+
+const DisplayContext = createContext<any>(initialState);
+
+const DisplayProvider = ({ children }: { children: ReactNode }) => {
+  const [displayState, dispatch] = useReducer(displayReducer, initialState);
+
+  const clear = () => {
+    return dispatch(clearData());
+  };
+  const type = (payload: string) => {
+    return dispatch(typeData(payload));
+  };
+
+  return (
+    <DisplayContext.Provider value={{ displayState, clear, type }}>
+      {children}
+    </DisplayContext.Provider>
+  );
+};
+const useDisplay = () => useContext(DisplayContext);
+
+export { DisplayProvider, useDisplay };
