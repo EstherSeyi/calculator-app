bc6c6a5461972cb0a422c39e965210147f36ec70
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-05 12:44:38 +0100

    temporary fix for the length of input that can be displayed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,18 @@
+import { useDisplay } from "../context/display";
+
 const accentBtn = ["reset", "del"];
 const largerBtn = ["reset", "="];
 
 const Button = ({ children, onClick }: { children: string; onClick: any }) => {
+  const { displayState } = useDisplay();
   return (
     <>
       <button
+        disabled={
+          !accentBtn.includes(children)
+            ? displayState?.displayData.length > 15
+            : false
+        }
         onClick={onClick}
         className={`py-2 micro:py-4 px-4 micro:px-6 rounded-md uppercase text-sm micro:text-base ${
           accentBtn.includes(children)
