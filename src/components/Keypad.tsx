f9e2f7f168f006510b2bc771bfc3193a604e1c66
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-05 12:20:23 +0100

    Adds calculate action to keypad equals key

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -4,33 +4,25 @@ import keys from "../constants/keys";
 import { useDisplay } from "../context/display";
 
 const Keypad = () => {
-  const { clear, type, reset } = useDisplay();
+  const { clear, type, reset, calculate, displayState } = useDisplay();
 
-  const handleType = (data: string) => {
-    type(data);
-  };
-
-  const handleClear = () => {
-    clear();
-  };
-
-  const handleReset = () => {
-    reset();
+  const handleClick = (value: string) => {
+    switch (value) {
+      case "del":
+        return clear();
+      case "reset":
+        return reset();
+      case "=":
+        return calculate(displayState.displayData);
+      default:
+        return type(value);
+    }
   };
 
   return (
     <div className="grid grid-cols-4 gap-x-2 gap-y-3 micro:gap-4  bg-bg-element p-4 rounded">
       {keys.map(({ value, label }, index) => (
-        <Button
-          key={`${value}_${index}`}
-          onClick={
-            value === "del"
-              ? handleClear
-              : value === "reset"
-              ? handleReset
-              : () => handleType(value)
-          }
-        >
+        <Button key={`${value}_${index}`} onClick={() => handleClick(value)}>
           {label}
         </Button>
       ))}
