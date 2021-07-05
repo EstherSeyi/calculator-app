4672257dc301257cb5c565e7e85f20dcedc593d2
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-05 15:34:40 +0100

    updates display text size

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -3,7 +3,7 @@ import { useDisplay } from "../context/display";
 const Display = () => {
   const { displayState } = useDisplay();
   return (
-    <div className="mb-4 bg-bg-screen py-4 px-3 rounded-md text-2xl text-misc-header">
+    <div className="mb-4 bg-bg-screen py-4 sm:py-5 px-3 rounded-md text-2xl sm:text-3xl md:text-4xl text-misc-header">
       <span className="ml-auto w-max block">
         {displayState.displayData === "" ? "0" : displayState.displayData}
       </span>
